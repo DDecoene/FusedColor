861d935e4cf9e219280299ac9e8de962ad9a1f1f
DDecoene <dennis.decoene@gmail.com>
2023-09-17 20:43:50 +0000

    code aligened with dev containers in vscode

--- fusedcolor/__main__.py
+++ fusedcolor/__main__.py
@@ -1,6 +1,6 @@
 """Entry point for fusedcolor."""
 
-from fusedcolor.cli import main  # pragma: no cover
+from fusedcolor.cli import main
 
 if __name__ == "__main__":  # pragma: no cover
     main()
