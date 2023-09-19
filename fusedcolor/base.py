9592f0fc11af0b40b8837b179920c7594c1c4085
Dennis Decoene <DDecoene@users.noreply.github.com>
2023-09-19 17:30:43 +0000

    better but not quite there

--- fusedcolor/base.py
+++ fusedcolor/base.py
@@ -29,6 +29,10 @@ def make_stencils(input_image_path: str, output_directory: str):
         # Calculate the opacity increment for each layer (25% opacity per layer)
         opacity_increment = 0.25
 
+        # reduce the number of colors in the image to the number of spray can colors
+        image = reduce_colors(image,num_layers)
+        cv2.imwrite(os.path.join(output_directory,'reduced.png'), image)
+        
         # Create stencils for each layer and spray can color
         for i, color in enumerate(spray_can_colors):
 
@@ -45,6 +49,30 @@ def make_stencils(input_image_path: str, output_directory: str):
             print(f"Stencil {i + 1} saved to {stencil_filename}")
 
         print("Stencil generation completed.")
+
+def reduce_colors(image, k):
+    # Reshape the image to a 2D array of pixels
+    pixels = image.reshape(-1, 3)
+
+    # Convert the pixels to float type
+    pixels = np.float32(pixels)
+
+    # Define criteria for k-means clustering
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+
+    # Perform k-means clustering
+    _, labels, centers = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # Convert the centers to unsigned 8-bit integer type
+    centers = np.uint8(centers)
+
+    # Replace each pixel value with its nearby center
+    segmented_image = centers[labels.flatten()]
+
+    # Reshape the segmented image back to its original shape
+    segmented_image = segmented_image.reshape(image.shape)
+
+    return segmented_image
 
 
 def group_pixels_nearest_color(image, target_color, threshold):
